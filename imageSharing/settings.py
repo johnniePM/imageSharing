370c2d78a0fbba064969fe76ff5d89006f42cfd6
johnniePM <johnnie.jpm@outlook.com>
2021-10-13 11:15:36 +0200

    setting updated

--- imageSharing/settings.py
+++ imageSharing/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'imageSharing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +118,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    Path.joinpath(BASE_DIR, "static")
+]
+MEDIA_ROOT= BASE_DIR/'media'
+MEDIA_URL='/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL='/'
+LOGOUT_REDIRECT_URL='/'
