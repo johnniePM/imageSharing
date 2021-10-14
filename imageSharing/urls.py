53e16429d5fdf13475330d4703e33dda07e4fbbb
antima <antimatyagi231@gmail.com>
2021-10-14 10:14:11 +0200

    git

--- imageSharing/urls.py
+++ imageSharing/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from imageSharing.settings import STATICFILES_DIRS
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include 
+import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('home.urls'))
 ]
