48f7b48f39febe58ce21255d28bc5c28b43079d3
johnniePM <johnnie.jpm@outlook.com>
2021-10-15 11:16:56 +0200

    urls

--- imageSetup/urls.py
+++ imageSetup/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ImageCreateFormView
+from .views import ImageCreateFormView,notes_list_view
 
 
 
 app_name="Images"
 urlpatterns = [
     path("create/",ImageCreateFormView.as_view(),name="create"),
+    path("",notes_list_view,name="list")
 ]
