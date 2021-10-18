a81719ce6b29f73f6f5f3214f4f185d585e4a916
antima <antimatyagi231@gmail.com>
2021-10-18 10:42:41 +0200

    linking images,user profile worked

--- imageSetup/urls.py
+++ imageSetup/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import ImageCreateFormView,notes_list_view,ImagesEditFormView
+from .views import ImageCreateFormView,images_list_view,ImagesEditFormView
 
 
 
-app_name="Images"
+app_name="images"
 urlpatterns = [
     path("create/",ImageCreateFormView.as_view(),name="create"),
-    path("",notes_list_view,name="list"),
+    path("",images_list_view,name="list"),
     path('edit_note/<int:pk>/',ImagesEditFormView.as_view(), name='edit_note'),
 
 ]
