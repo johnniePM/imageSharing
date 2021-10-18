156220bdbb3e6157d8caa3edd0735302d7482aaf
johnniePM <johnnie.jpm@outlook.com>
2021-10-18 11:44:41 +0200

    urls error fix in the image setup

--- imageSetup/urls.py
+++ imageSetup/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ImageCreateFormView,images_list_view,ImagesEditFormView
+from .views import ImageCreateFormView,images_list_view,ImagesEditFormView,ImageDeleteView,ImagesDetailView
 
 
 
@@ -7,8 +7,6 @@ app_name="images"
 urlpatterns = [
     path("create/",ImageCreateFormView.as_view(),name="create"),
     path("",images_list_view,name="list"),
-    path('edit_note/<int:pk>/',ImagesEditFormView.as_view(), name='edit_note'),
-    path("",notes_list_view,name="list"),
     path('edit_image/<int:pk>/',ImagesEditFormView.as_view(), name='edit_image'),
     path("<int:pk>", ImagesDetailView.as_view(),name="details"),
     path('<int:pk>/imageDelete/', ImageDeleteView.as_view(), name='image_delete'),
