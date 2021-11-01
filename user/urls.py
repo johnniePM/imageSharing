096948c218de5bce6f65a26b0e54b06325f8643a
johnniePM <johnnie.jpm@outlook.com>
2021-11-01 12:44:25 +0100

    oops forgot to commit the views

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,12 @@
 from django import views
 from django.urls import path
 from .import  views
-from .views import ProfileEditFormView
+from .views import ProfileEditFormView,UserListView
 
 app_name="user"
 urlpatterns = [
     path('<str:userName>',views.profile_view,name="profile"),
     # path('',views.profile_view,name="profile"),
     path('edit_profile/<str:user_id>/',ProfileEditFormView.as_view(), name='edit_profile'),
+    path('user_list/',UserListView.as_view(),name='user_list')
 ]
